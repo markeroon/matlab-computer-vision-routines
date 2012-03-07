233d3d82019cbe7aeb7fe695c985dfb313004484
Mark <markeroon@gmail.com>
2012-03-07 01:55:05 -0500

    More dev -- the CPD method doesn't really work at all on the plant data.

--- TumorRegistration/main.m
+++ TumorRegistration/main.m
@@ -1,4 +1,5 @@
 data = open('../../Data/DeformStaple_allpatients.mat')
+addpath('../PointCloudGenerator' );
 addpath(genpath('../third_party/CPD2/'));
 addpath('../plant_registration');
 segmentation = data.rois_patient001;
@@ -19,8 +20,29 @@ for i=1:num_segmentations
     
 end
 
+iters_rigid = 30;
+iters_nonrigid = 0;
+Yr_subdiv = ones(size(X{212}));
+[Yr_subdiv(:,1),Yr_subdiv(:,2),Yr_subdiv(:,3)] = register_surface_subdivision_upper_bound( ...
+                                           X{20},X{212},iters_rigid,iters_nonrigid );
+
+[neighbour_id,neighbour_dist] = kNearestNeighbors(X{20}, Yr_subdiv, 1 );
+% get nearest neighbour for each point in the original cloud in the
+% matched cloud
+
+neighbour_id_u = unique(neighbour_id);
+
+Y_reg = ones(size(X{20}(neighbour_id_u,:)));
+[Y_reg(:,1),Y_reg(:,2),Y_reg(:,3)] = register_surface_subdivision_upper_bound( ...
+                                           X{212},X{20}(neighbour_id_u,:),iters_rigid,iters_nonrigid );
+                                       
+[neighbour_id_reg,neighbour_dist_reg] = kNearestNeighbors(Y_reg, X{20}(neighbour_id_u,:), 1 );
+sprintf('RMS-E: ' )
+rms_e = sqrt( sum(neighbour_dist_reg(:))/ length(neighbour_dist_reg(:)) ) 
+
+
 iters_rigid = 0;
-iters_nonrigid = 70;
+iters_nonrigid = 20;
 lambda = 3;%13; % regularization weight
 beta = .1;%10; % width of gaussian
 Y = ones(size(X{212}));
@@ -29,6 +51,22 @@ Y = ones(size(X{212}));
                                   iters_nonrigid, lambda,...
                                   beta, 1);
 
+% now matched targeted points back onto original scan.                              
+idx_unique = unique(c_nr);                         
+Xmatched = X{20}(idx_unique,:);
+Ymatched = ones(size(X{20}(idx_unique,:)));
+[Ymatched(:,1),Ymatched(:,2),Ymatched(:,3),t_r1,t_nr1,c_r1,c_nr1] = ...
+    registerToReferenceRangeScan(X{20}, Xmatched, iters_rigid, ...                                                iters_rigid,...
+                                  iters_nonrigid, lambda,...
+                                  beta, 1);
+%OVERLAP RMS-E
+% take explicit matches and bring them back
+dist_x = X{20}(c_nr1,1) - Ymatched(:,1);
+dist_y = X{20}(c_nr1,2) - Ymatched(:,2);
+dist_z = X{20}(c_nr1,3) - Ymatched(:,3);
+dist = sqrt( dist_x.^2 + dist_y.^2 + dist_z.^2 );
+rms = sqrt( sum(dist(:)) / length(dist(:)))
+%{                              
 lambda = 3;
 beta = .1;
 Y_ = ones(size(X{212}));
@@ -37,7 +75,15 @@ Y_ = ones(size(X{212}));
                                   iters_nonrigid, lambda,...
                                   beta, 1);
 
-                                                   
+dist_x  = X{212}(:,1) - Y_(c_nr2,1);
+dist_y = X{212}(:,2) - Y_(c_nr2,2);
+dist_z = X{212}(:,3) - Y_(c_nr2,3);
+
+dist = sqrt( dist_x.^2 + dist_y.^2 + dist_z.^2 );
+rms = sum(dist(:))/sqrt(length(dist))
+%}
+
+
 %{
 b = rois_patient001{2};
 b_x2=[];b_y2=[];b_z2=[];
--- plant_registration_nonrigid/register_nonrigid_main.m
+++ plant_registration_nonrigid/register_nonrigid_main.m
@@ -3,24 +3,43 @@ addpath( '../file_management/' );
 addpath( genpath( '../third_party/CoherentPointDrift' ) );
 addpath( '../plant_registration' );
 
-filename_0 = sprintf( '~/Data/PlantDataPly/plants_converted82-%03d-clean.ply', 0 );
+filename_0 = sprintf( '~/Data/PlantDataPly/plants_converted82-%03d-clean_cut.ply', 3 );
 [Elements_0,varargout_0] = plyread(filename_0);
 X = [Elements_0.vertex.x';Elements_0.vertex.y';Elements_0.vertex.z']';
     
-filename_1 = sprintf( '~/Data/PlantDataPly/plants_converted82-%03d-clean.ply', 1 );
+filename_1 = sprintf( '~/Data/PlantDataPly/plants_converted82-%03d-clean_cut.ply', 4 );
 [Elements_1,varargout_1] = plyread(filename_1);
 Y = [Elements_1.vertex.x';Elements_1.vertex.y';Elements_1.vertex.z']';
 
-iters_rigid = 3;
+iters_rigid = 1;
 iters_nonrigid = 0;
-[X_new,Y_new,Z_new] = register_surface_subdivision_upper_bound( ...
-                                           X,Y,iters_rigid,iters_nonrigid );
+Yr_subdiv = ones(size(Y));
 
+[Yr_subdiv(:,1),Yr_subdiv(:,2),Yr_subdiv(:,3),corr_r] = register_surface_subdivision_upper_bound( ...
+                                           X,Y,iters_rigid,iters_nonrigid );
+                                       
+[neighbour_id,neighbour_dist] = kNearestNeighbors(X, Y, 1 );
+% get nearest neighbour for each point in the original cloud in the
+% matched cloud
+
+
+
+Y_reg = ones(size(X(neighbour_id,:)));
+[Y_reg(:,1),Y_reg(:,2),Y_reg(:,3)] = register_surface_subdivision_upper_bound( ...
+                                          Y,X(neighbour_id,:),iters_rigid,iters_nonrigid );
+                                       
+[neighbour_id_reg,neighbour_dist_reg] = kNearestNeighbors(Y_reg, X{20}, 1 );
+sprintf('RMS-E: ' )
+rms_e = sqrt( sum(neighbour_dist_reg(:))/ length(neighbour_dist_reg(:)) )                      
+
+                                       
 %{
-[Y1_,Y2_,Y3_] = registerToReferenceRangeScan(X, Y, 100, ...                                                iters_rigid,...
-                                                       0,...
-                                                       1,...
-                                                       1,...
-                                                       1);
-%}
+lambda = 3;
+beta = 0.1;
+[Y1_,Y2_,Y3_] = registerToReferenceRangeScan(X, Y, 0, ...                                                iters_rigid,...
+                                                    5,...
+                                                    lambda,...
+                                                    beta,...
+                                                    2);
                                                    
+%}
