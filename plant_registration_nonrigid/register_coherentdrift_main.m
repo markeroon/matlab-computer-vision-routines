5fd545eb241ae375b9647b3e942e4c8ac1b62dab
Mark <markeroon@gmail.com>
2012-03-28 22:28:59 -0400

    File saving works.

---
+++ plant_registration_nonrigid/register_coherentdrift_main.m
@@ -0,0 +1,64 @@
+addpath( '../file_management/' );
+addpath( genpath( '../third_party/CPD2' ) );
+addpath( '../plant_registration' );
+addpath( '../PointCloudGenerator' );
+rms_e_all = [];
+R =  [ 0.9101   -0.4080    0.0724 ;
+       0.4118    0.8710   -0.2681 ;
+       0.0463    0.2738    0.9607 ];
+t = [ 63.3043,  234.5963, -46.8392 ];
+
+
+filename_0 = sprintf( '../../Data/PlantDataPly/plants_converted82-%03d-clean-clear.ply', 0 );
+[Elements_0,varargout_0] = plyread(filename_0);
+X = [Elements_0.vertex.x';Elements_0.vertex.y';Elements_0.vertex.z']';
+X = X(1:40:end,:); % subset for testing
+Y_reg_all = [];
+for q=1:11
+filename_1 = sprintf( '../../Data/PlantDataPly/plants_converted82-%03d-clean-clear.ply', q );
+[Elements_1,varargout_1] = plyread(filename_1);
+Y = [Elements_1.vertex.x';Elements_1.vertex.y';Elements_1.vertex.z']';
+
+for j=1:q
+        Y_dash = R*Y' + repmat(t,size(Y,1),1)';
+        Y = Y_dash';
+end
+
+
+
+Y = Y(1:40:end,:); %subset for testing
+opt.viz = 1;
+opt.max_it = 70;
+opt.rotation = 1;
+opt.scale = 0;
+opt.normalize = 1;
+opt.fgt = 2;
+opt.lambda = 2;
+opt.method = 'nonrigid_lowrank';
+opt.beta = 1; %possible that less than this is too much ram
+%min_size = 500;
+
+[T] = cpd_register(X,Y,opt);
+Yr_cpd = T.Y;  
+
+[neighbour_id] = kNearestNeighbors(X, Yr_cpd, 1 );
+% get nearest neighbour for each point in the original cloud in the
+% matched cloud
+filename = sprintf( '../../Data/Yr_cpd%02d-%s.mat',[q datestr(now,'dd.mm.yy.HH.MM') ] )
+save(filename,'Yr_cpd');
+neighbour_id_unique = unique(neighbour_id);
+Yr_cpd = [];
+T = cpd_register( Y,X(neighbour_id_unique,:), opt );
+X_reg = T.Y;
+
+neighbour_id=[];
+neighbour_dist=[];
+[neighbour_id_reg,neighbour_dist_reg] = ...
+                    kNearestNeighbors(X_reg, X, 1 );
+sprintf('RMS-E: ' )
+rms_e = sqrt( sum(neighbour_dist_reg(:)) / length(neighbour_dist_reg(:)) )
+rms_e_all = [rms_e_all rms_e];
+neighbour_id_reg=[];
+neighbour_dist_reg=[];
+%Y_reg_all = [Y_reg_all ; Yr_subdiv];
+end
