d14cae9bb68e6e7df9f547dab03c790a2bcc3da3
unknown <mbrophy5@elvex.nt.csd.uwo.ca>
2012-03-28 20:09:53 -0400

    registerRecursive. Lots of simplifications; removed dependency on registerToReferenceRangeScan

--- plant_registration_nonrigid/register_nonrigid_main.m
+++ plant_registration_nonrigid/register_nonrigid_main.m
@@ -1,6 +1,7 @@
 addpath( '../file_management/' );
 addpath( genpath( '../third_party/CPD2' ) );
 addpath( '../plant_registration' );
+addpath( '../PointCloudGenerator' );
 rms_e_all = [];
 R =  [ 0.9101   -0.4080    0.0724 ;
        0.4118    0.8710   -0.2681 ;
@@ -8,13 +9,13 @@ R =  [ 0.9101   -0.4080    0.0724 ;
 t = [ 63.3043,  234.5963, -46.8392 ];
 
 
-filename_0 = sprintf( '~/Data/PlantDataPly/plants_converted82-%03d-clean-clear-reduced.ply', 0 );
+filename_0 = sprintf( '../../Data/PlantDataPly/plants_converted82-%03d-clean-clear.ply', 0 );
 [Elements_0,varargout_0] = plyread(filename_0);
 X = [Elements_0.vertex.x';Elements_0.vertex.y';Elements_0.vertex.z']';
-%X = X(1:10:end,:); % subset for testing
+%X = X(1:40:end,:); % subset for testing
 Y_reg_all = [];
 for q=1:11
-filename_1 = sprintf( '~/Data/PlantDataPly/plants_converted82-%03d-clean-clear-reduced.ply', q );
+filename_1 = sprintf( '../../Data/PlantDataPly/plants_converted82-%03d-clean-clear.ply', q );
 [Elements_1,varargout_1] = plyread(filename_1);
 Y = [Elements_1.vertex.x';Elements_1.vertex.y';Elements_1.vertex.z']';
 
@@ -25,16 +26,16 @@ end
 
 
 
-%Y = Y(1:10:end,:); %subset for testing
-
-%opt.max_it = 40;
+%Y = Y(1:40:end,:); %subset for testing
+opt.viz = 0;
+opt.max_it = 70;
 opt.rotation = 1;
 opt.scale = 0;
 opt.normalize = 1;
 opt.fgt = 2;
 %opt.lambda = 1;
 %opt.beta = .1; %possible that less than this is too much ram
-min_size = 120;
+min_size = 500;
 Yr_subdiv = ones(size(Y));
 
 [Yr_subdiv(:,1),Yr_subdiv(:,2),Yr_subdiv(:,3)] =  ...      
@@ -43,22 +44,26 @@ Yr_subdiv = ones(size(Y));
                     %                       X,Y,iters_rigid,iters_nonrigid,...
                     %                       lambda,beta, min_size );
                                        
-[neighbour_id,neighbour_dist] = kNearestNeighbors(X, Yr_subdiv, 1 );
+[neighbour_id] = kNearestNeighbors(X, Yr_subdiv, 1 );
 % get nearest neighbour for each point in the original cloud in the
 % matched cloud
-
-
+filename = sprintf( '../../Data/Yr_subdiv%02d-%s.mat',[q datestr(now,'dd.mm.yy.HH.MM') ] )
+save( Yr_subdiv,filename )
 neighbour_id_unique = unique(neighbour_id);
-
+Yr_subdiv = [];
 X_reg = ones(size(X(neighbour_id_unique,:)));
-[X_reg(:,1),X_reg(:,2),X_reg(:,3)] = register_surface_subdivision_upper_bound( ...
-                                          Y,X(neighbour_id_unique,:),iters_rigid,...
-                                          iters_nonrigid,lambda, beta, min_size );
-                                       
+[X_reg(:,1),X_reg(:,2),X_reg(:,3)] = registerRecursive( ...
+                                          Y,X(neighbour_id_unique,:),...
+                                          opt, min_size );
+
+neighbour_id=[];
+neighbour_dist=[];
 [neighbour_id_reg,neighbour_dist_reg] = ...
                     kNearestNeighbors(X_reg, X, 1 );
 sprintf('RMS-E: ' )
 rms_e = sqrt( sum(neighbour_dist_reg(:)) / length(neighbour_dist_reg(:)) )
 rms_e_all = [rms_e_all rms_e];
-Y_reg_all = [Y_reg_all ; Yr_subdiv];
+neighbour_id_reg=[];
+neighbour_dist_reg=[];
+%Y_reg_all = [Y_reg_all ; Yr_subdiv];
 end
