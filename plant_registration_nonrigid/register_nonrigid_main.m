ca6b8d949b72045771ec3adec2e5580d618de31d
Mark B <markeroon@gmail.com>
2012-06-14 15:22:28 -0400

    Back to gmms combined with knn for points

--- plant_registration_nonrigid/register_nonrigid_main.m
+++ plant_registration_nonrigid/register_nonrigid_main.m
@@ -13,11 +13,7 @@ t = [ 63.3043,  234.5963, -46.8392 ];
 filename_0 = sprintf( '../../Data/PlantDataPly/plants_converted82-%03d-clean-clear.ply', 0 );
 [Elements_0,varargout_0] = plyread(filename_0);
 X = [Elements_0.vertex.x';Elements_0.vertex.y';Elements_0.vertex.z']';
-<<<<<<< HEAD
-%X = X(1:15:end,:); % subset for testing
-=======
-X = X(1:30:end,:); % subset for testing
->>>>>>> d562008472380cc99831f579c489b783444ad159
+
 Y_reg_all = cell(11,1);
 for q=1:11
 filename_1 = sprintf( '../../Data/PlantDataPly/plants_converted82-%03d-clean-clear.ply', q );
@@ -30,16 +26,9 @@ for j=1:q
 end
 
 
-
-<<<<<<< HEAD
-%Y = Y(1:15:end,:); %subset for testing
-opt.viz = 0;
-=======
-Y = Y(1:30:end,:); %subset for testing
 opt.viz = 1;
->>>>>>> d562008472380cc99831f579c489b783444ad159
-opt.max_it = 20;
-opt.outliers = 0.2;
+opt.max_it = 15;
+opt.outliers = 0.1;
 opt.tol = 1e-12;
 opt.rot = 0;
 opt.normalize = 1;
@@ -48,26 +37,23 @@ opt.fgt = 2;
 opt.method='rigid';
 %opt.lambda = 7;
 %opt.beta = 2; %possible that less than this is too much ram
-<<<<<<< HEAD
-min_size = 9000;
-=======
-min_size = 140;
+min_size = 500;
 max_registerable_dist = 12;
->>>>>>> d562008472380cc99831f579c489b783444ad159
-Yr_subdiv = ones(size(Y));
+%Yr_subdiv = ones(size(Y));
 
-[Yr_subdiv(:,1),Yr_subdiv(:,2),Yr_subdiv(:,3)] =  ...      
+[Yr_subdiv_x,Yr_subdiv_y,Yr_subdiv_z] =  ...      
     registerRecursive( X,Y,opt,min_size,max_registerable_dist );
                     %  register_surface_subdivision_upper_bound( ...
                     %                       X,Y,iters_rigid,iters_nonrigid,...
                     %                       lambda,beta, min_size );
             
                     
-[neighbour_id] = kNearestNeighbors(X, Yr_subdiv, 1 );
+%[neighbour_id] = kNearestNeighbors(X, Yr_subdiv, 1 );
 % get nearest neighbour for each point in the original cloud in the
 % matched cloud
 filename = sprintf( '../../Data/Yr_subdiv%02d-%s.mat',[q datestr(now,'dd.mm.yy.HH.MM') ] )
 save(filename,'Yr_subdiv');
+%{
 neighbour_id_unique = unique(neighbour_id);
 Y_reg_all{q} = Yr_subdiv;
 Yr_subdiv = [];
@@ -85,5 +71,6 @@ rms_e = sqrt( sum(neighbour_dist_reg(:)) / length(neighbour_dist_reg(:)) )
 rms_e_all = [rms_e_all rms_e];
 neighbour_id_reg=[];
 neighbour_dist_reg=[];
-%Y_reg_all = [Y_reg_all ; Yr_subdiv];
+%}
+Y_reg_all{q} = Yr_subdiv;
 end
